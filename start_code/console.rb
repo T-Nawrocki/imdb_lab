56d3544209963cdb4e7454a6fc9e4af9872e11f1
Helena <inger.helena@gmail.com>
2020-07-02 14:23:40 +0100

    created movie class

--- start_code/console.rb
+++ start_code/console.rb
@@ -1,4 +1,8 @@
 require_relative("models/star")
+require_relative("models/movie")
 
 star_1 = Star.new({"first_name" => "George", "last_name" => "Clooney"})
 star_1.save
+
+movie1 = Movie.new({"title" => "Batman and Robin", "genre" => "superhero"})
+movie1.save
--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -1,3 +1,21 @@
 class Movie
 
+    attr_reader :id
+    attr_accessor :title, :genre
+
+    def initialize(options)
+        @id = options['id'] if options['id']
+        @title = options['title']
+        @genre = options['genre']
+    end
+
+    def save
+        sql = "INSERT INTO movies
+        (title, genre) VALUES
+        ($1, $2) RETURNING id"
+        values = [@title, @genre]
+        result = SqlRunner.run(sql, values)
+        @id = result[0]['id'].to_i
+    end
+
 end
